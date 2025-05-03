af3b5b43cf3e802edadcc0bb74986b219b1cee6e
Alex Korzh <alex@korzh.me>
2025-05-03 14:06:32 +0200

    Documentation improved

--- epx.el
+++ epx.el
@@ -29,7 +29,10 @@
 ;; It stores commands in .dir-locals.el or any other dir-locals-file that
 ;; you set. It allows you to add or remove commands, no editing
 ;; capabilities for now. You can choose whether to use compilation
-;; buffer when you create your command.
+;; buffer when you create your command. After the command is created,
+;; you can execute it using ’epx-run-command-in-shell’ (You’ll probably
+;; want to bind it). Completion for command names is provided. Executing
+;; a command happens in a separate window (either ’shell’ or compilation).
 
 ;;; Code:
 (require 'project)
